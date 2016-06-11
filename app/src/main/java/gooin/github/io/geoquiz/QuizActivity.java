556110bae592614dda56589aff2de77bd163f8d4
gooin <gooin@outlook.com>
2016-06-11 19:03:36 +0800

    简单布局搞定，Toast可用

--- app/src/main/java/gooin/github/io/geoquiz/QuizActivity.java
+++ app/src/main/java/gooin/github/io/geoquiz/QuizActivity.java
@@ -2,12 +2,36 @@ package gooin.github.io.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //R.string.correct_toast   调用string里面的值，感觉方便多语言吧
+                Toast.makeText(QuizActivity.this,R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //R.string.correct_toast   调用string里面的值，感觉方便多语言吧
+                Toast.makeText(QuizActivity.this,R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
